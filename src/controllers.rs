84f21e16e995feacc5dab5f8b48bf553147316f4
zilard <zilard.cherry@gmail.com>
2022-04-27 21:47:21 +0200

    formatting Rust code

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,17 +1,15 @@
 use actix_web::{web, HttpResponse, Responder};
-use mongodb::{options::FindOptions, Client};
-use bson::{ doc, oid };
-use std::sync::*;
+use bson::{doc, oid};
 use futures::stream::StreamExt;
+use mongodb::{options::FindOptions, Client};
 use serde::{Deserialize, Serialize};
-
+use std::sync::*;
 
 #[derive(Serialize, Deserialize)]
 pub struct Flight {
     pub content: String,
     pub is_done: bool,
 }
-
 
 #[derive(Serialize)]
 struct Response {
@@ -22,25 +20,23 @@ const MONGO_DB: &'static str = "flightdb";
 const MONGO_COLLECTION: &'static str = "flightdb";
 
 /*
-We imported the needed modules, 
-created two structs Flight and Response and two const variables, 
+We imported the needed modules,
+created two structs Flight and Response and two const variables,
 
-The Flight struct is responsible for how model data would be inputted 
-into the database, 
+The Flight struct is responsible for how model data would be inputted
+into the database,
 
-The Response handles how response messages would be sent back on an endpoint. 
+The Response handles how response messages would be sent back on an endpoint.
 
-The MONGO_DB and MONGOCOLLECTION holds the constant strings of our 
+The MONGO_DB and MONGOCOLLECTION holds the constant strings of our
 database name and collection name.
 */
 
-
 /*
-These are the handlers for each route, 
-they are each asynchronous functions that return a Responder trait 
+These are the handlers for each route,
+they are each asynchronous functions that return a Responder trait
 provided by actix-web
 */
-
 
 /*
 pub trait Responder {
@@ -50,24 +46,17 @@ pub trait Responder {
 }
 */
 
-pub async fn get_flights(
-    data: web::Data<Mutex<Client>>
-) -> impl Responder {
-
+pub async fn get_flights(data: web::Data<Mutex<Client>>) -> impl Responder {
     let flight_collection = data
-                            .lock()
-                            .unwrap()
-                            .database(MONGO_DB)
-                            .collection(MONGO_COLLECTION);
+        .lock()
+        .unwrap()
+        .database(MONGO_DB)
+        .collection(MONGO_COLLECTION);
 
     let filter = doc! {};
-    let find_options = FindOptions::builder()
-                       .sort(doc! { "_id": -1 })
-                       .build();
+    let find_options = FindOptions::builder().sort(doc! { "_id": -1 }).build();
 
-    let mut cursor = flight_collection.find(filter, find_options)
-                                      .await
-                                      .unwrap();
+    let mut cursor = flight_collection.find(filter, find_options).await.unwrap();
 
     let mut results = Vec::new();
 
@@ -77,41 +66,37 @@ pub async fn get_flights(
                 results.push(document);
             }
             _ => {
-                return HttpResponse::InternalServerError()
-                                     .finish();
+                return HttpResponse::InternalServerError().finish();
             }
         }
     }
     HttpResponse::Ok().json(results)
 
-/*
-    format!("Fetch all flights");
-    HttpResponse::Ok()
-        .body("Fetch all flights")
-*/
-
+    /*
+        format!("Fetch all flights");
+        HttpResponse::Ok()
+            .body("Fetch all flights")
+    */
 }
-
-
-
 
 pub async fn create_flight(
     data: web::Data<Mutex<Client>>,
-    flight: web::Json<Flight>
+    flight: web::Json<Flight>,
 ) -> impl Responder {
-
     let flight_collection = data
-                            .lock()
-                            .unwrap()
-                            .database(MONGO_DB)
-                            .collection(MONGO_COLLECTION);
+        .lock()
+        .unwrap()
+        .database(MONGO_DB)
+        .collection(MONGO_COLLECTION);
 
-    match flight_collection.insert_one(
-        doc! {"content": &flight.content,
-              "is_done": &flight.is_done},
-        None)
-        .await {
-
+    match flight_collection
+        .insert_one(
+            doc! {"content": &flight.content,
+            "is_done": &flight.is_done},
+            None,
+        )
+        .await
+    {
         Ok(db_result) => {
             if let Some(new_id) = db_result.inserted_id.as_object_id() {
                 println!("New data inserted with id {}", new_id);
@@ -120,76 +105,64 @@ pub async fn create_flight(
                 message: "Successful".to_string(),
             };
             return HttpResponse::Created().json(response);
-
         }
         Err(err) => {
             println!("Failed! {}", err);
             return HttpResponse::InternalServerError().finish();
         }
-    } 
+    }
 
-/*
-    format!("Create a new flight");
-    HttpResponse::Ok()
-        .body("Create a new flight")
-*/
-
-
+    /*
+        format!("Create a new flight");
+        HttpResponse::Ok()
+            .body("Create a new flight")
+    */
 }
 
-
-
-
-
 pub async fn fetch_one(
-    data: web::Data<Mutex<Client>>, 
-    flight_id: web::Path<String>
+    data: web::Data<Mutex<Client>>,
+    flight_id: web::Path<String>,
 ) -> impl Responder {
-
     let flight_collection = data
-                            .lock()
-                            .unwrap()
-                            .database(MONGO_DB)
-                            .collection(MONGO_COLLECTION);
+        .lock()
+        .unwrap()
+        .database(MONGO_DB)
+        .collection(MONGO_COLLECTION);
 
     let filter = doc! {
-        "_id": oid::ObjectId:with_string(&flight_id.to_string()).unwrap() };
+    "_id": oid::ObjectId:with_string(&flight_id.to_string()).unwrap() };
     let obj = flight_collection.find_one(filter, None).await.unwrap();
 
     return HttpResponse::Ok().json(obj);
 
-/*
-    format!("Fetch one flight data");
-    HttpResponse::Ok()
-        .body("Fetch one flight data")
-*/
-
-
+    /*
+        format!("Fetch one flight data");
+        HttpResponse::Ok()
+            .body("Fetch one flight data")
+    */
 }
-
-
-
-
 
 pub async fn update_flight(
     data: web::Data<Mutex<Client>>,
     flight_id: web::Path<String>,
-    flight: web::Json<Flight>
+    flight: web::Json<Flight>,
 ) -> impl Responder {
-
     let flight_collection = data
-                            .lock()
-                            .unwrap()
-                            .database(MONGO_DB)
-                            .collection(MONGO_COLLECTION);
+        .lock()
+        .unwrap()
+        .database(MONGO_DB)
+        .collection(MONGO_COLLECTION);
     let filter = doc! {
-        "_id": oid::ObjectId::with_string(&flight_id.to_string()).unwrap() };
+    "_id": oid::ObjectId::with_string(&flight_id.to_string()).unwrap() };
 
-    let data = doc! { 
-        "$set": { "content": &flight.content, 
-                  "is_done": &flight.is_done } };
+    let data = doc! {
+    "$set": { "content": &flight.content,
+              "is_done": &flight.is_done } };
 
-    flight_collection.update_one(filter, data, None).await.unwrap();
+    flight_collection
+        .update_one(filter, data, None)
+        .await
+        .unwrap();
 
     let response = Response {
         message: "Updated Successfully".to_string(),
@@ -197,49 +170,33 @@ pub async fn update_flight(
 
     return HttpResponse::Ok().json(response);
 
-
-
-
-/*
-    format!("Update a flight");
-    HttpResponse::Ok()
-        .body("Update a flight")
-*/
-
+    /*
+        format!("Update a flight");
+        HttpResponse::Ok()
+            .body("Update a flight")
+    */
 }
 
-
-
-
-
-
-
-
-
-
 pub async fn delete_flight(
-    data: web::Data<Mutex<Client>>, 
-    flight_id: web::Path<String>
+    data: web::Data<Mutex<Client>>,
+    flight_id: web::Path<String>,
 ) -> impl Responder {
-
     let flight_collection = data
-                            .lock()
-                            .unwrap()
-                            .database(MONGO_DB)
-                            .collection(MONGO_COLLECTION);
+        .lock()
+        .unwrap()
+        .database(MONGO_DB)
+        .collection(MONGO_COLLECTION);
 
     let filter = doc! {
-        "_id": oid::ObjectId::with_string(&flight_id.to_string()).unwrap() };
+    "_id": oid::ObjectId::with_string(&flight_id.to_string()).unwrap() };
 
     flight_collection.delete_one(filter, None).await.unwrap();
 
     return HttpResponse::NoContent();
 
-/*
-    format!("Delete a flight");
-    HttpResponse::Ok()
-        .body("Delete a flight")
-*/
-
+    /*
+        format!("Delete a flight");
+        HttpResponse::Ok()
+            .body("Delete a flight")
+    */
 }
-
--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,26 @@
 use actix_web::{web, App, HttpServer};
 //use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-
-use mongodb::{Client, options::ClientOptions};
+use mongodb::{options::ClientOptions, Client};
 use std::sync::*;
 
-
 mod controllers;
-
 
 // we use the attribute #[actix_rt::main] to ensure it’s executed with the actix runtime
 
 // creates a MongoDB client that is wrapped in a Mutex for thread safety which is then passed into the app state to be used by our controllers.
 
-
-
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=debug");
 
-    let mut client_options = ClientOptions::parse(
-        "mongodb://127.0.0.1:27017/flightdb").await.unwrap();
+    let mut client_options = ClientOptions::parse("mongodb://127.0.0.1:27017/flightdb")
+        .await
+        .unwrap();
 
     client_options.app_name = Some("Flights".to_string());
 
-    let client = web::Data::new(
-        Mutex::new(
-            Client::with_options(client_options).unwrap()));
+    let client = web::Data::new(Mutex::new(Client::with_options(client_options).unwrap()));
 
     HttpServer::new(move || {
         App::new()
